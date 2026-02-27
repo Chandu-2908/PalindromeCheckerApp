0f093abd8efa1345da0442f8a18ce58f84c5ba1c
Veeramalla Chandu <chanduveeramalla113@gmail.com>
2026-02-27 14:46:43 +0530

    [chandu] Added usecase1 PalindromeChecker

---
+++ PalindromCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("palindrome checker app");
+        System.out.println("version 1.0");  
+    }
+}
